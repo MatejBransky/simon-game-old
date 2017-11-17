e68799e59e59adac3deeb806c40bff1645427ade
Matej Mazur <matej.mazur@mail.com>
2017-11-17 23:38:03 +0100

    Add first components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import {
+  ColorButton,
+  Panel,
+  Display,
+  StartButton,
+  StrictButton,
+  PowerSwitch
+} from './Components'
 
 class App extends Component {
+  state = {
+    power: false,
+    start: false,
+    strict: false,
+    display: {
+      status: false,
+      content: ''
+    }
+  }
+
   render() {
+    const { power, strict, display } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Game">
+          <ColorButton color="green" position="top-left" />
+          <ColorButton color="red" position="top-right" />
+          <ColorButton color="yellow" position="bottom-left" />
+          <ColorButton color="blue" position="bottom-right" />
+          <Panel>
+            <h1>Simon</h1>
+            <Display status={display.status}>
+              {display.content}
+            </Display>
+            <StartButton />
+            <StrictButton strict={strict} />
+            <PowerSwitch power={power} />
+          </Panel>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
