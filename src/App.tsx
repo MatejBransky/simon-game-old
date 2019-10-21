56d8a4507f270db3faccf64343b3c8fc26ab303e
Matej Bransky <bransky@logio.cz>
2019-10-21 18:57:11 +0200

    add css in jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import css from '@emotion/css';
 
 const App = () => {
   const [value, setValue] = useState(1);
 
   return (
-    <div>
+    <div css={styles}>
       <h1>Hello World! {value}</h1>
       <button onClick={() => setValue(value + 1)}>Increment</button>
     </div>
   );
 };
+
+const styles = css`
+  background: lightgray;
+
+  h1 {
+    color: red;
+  }
+`;
 
 export default App;
