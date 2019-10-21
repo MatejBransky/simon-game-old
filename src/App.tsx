20843032461ff16dc726df6a3c825677d188ec9d
Matej Bransky <bransky@logio.cz>
2019-10-21 20:33:23 +0200

    remove css-in-jsx / use instead sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React, { useState } from 'react';
-import css from '@emotion/css';
+import React, { useRef, createRef, RefObject } from 'react';
+
+import './App.scss';
 
 const App = () => {
-  const [value, setValue] = useState(1);
+  const refs = useRef<RefObject<HTMLButtonElement>[]>(
+    [...Array(colors.length)].map(() => createRef())
+  );
 
   return (
-    <div css={styles}>
-      <h1>Hello World! {value}</h1>
-      <button onClick={() => setValue(value + 1)}>Increment</button>
+    <div className="game">
+      <div className="buttons">
+        {colors.map((color, index) => (
+          <button
+            key={color}
+            className={color}
+            data-testid={`button-${color}`}
+            ref={refs.current[index]}
+          ></button>
+        ))}
+      </div>
+      <div className="dashboard">
+        <h2>Simon</h2>
+        <div className="controls">
+          <section className="display">
+            <div className="display">TODO</div>
+            <p>Count</p>
+          </section>
+
+          <section className="start">
+            <button></button>
+            <p>Start</p>
+          </section>
+
+          <section className="strict">
+            <button></button>
+            <p>Strict</p>
+          </section>
+
+          <section className="power">
+            <p className="off">Off</p>
+            <p className="on">On</p>
+            <button></button>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
 
-const styles = css`
-  background: lightgray;
-
-  h1 {
-    color: red;
-  }
-`;
+const colors = ['red', 'green', 'blue', 'yellow'];
 
 export default App;
