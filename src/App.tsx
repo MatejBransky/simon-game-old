82463f2201fc6adeb3d4a1b446f2a1ae591d21a6
Matej Bransky <bransky@logio.cz>
2019-10-21 16:52:12 +0200

    fix eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App = () => <div>Hello World!</div>;
+const App = () => {
+  const [value, setValue] = useState(1);
+
+  return (
+    <div>
+      <h1>Hello World! {value}</h1>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+    </div>
+  );
+};
 
 export default App;
